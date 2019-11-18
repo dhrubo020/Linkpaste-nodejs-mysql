b4b83479d32da4fcf615271bae66278960981f06
dhrubo020 <44555051+dhrubo020@users.noreply.github.com>
2019-11-18 20:48:44 +0600

    updated

--- linkpaste.sql
+++ linkpaste.sql
@@ -1,32 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 17, 2019 at 02:46 PM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `linkpaste`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `message`
---
 
 CREATE TABLE `message` (
   `Id` int(11) NOT NULL,
@@ -36,20 +8,6 @@ CREATE TABLE `message` (
   `DateTime` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `message`
---
-
-INSERT INTO `message` (`Id`, `Text`, `From`, `To`, `DateTime`) VALUES
-(29, 'date.toLocaleString()', 'dhrubo', 'dhrubo', '9/17/2019, 6:24:33 PM'),
-(30, 'date.toLocaleString()', 'ami', 'ami', '9/17/2019, 6:25:53 PM'),
-(31, 'first message', 'dhrubo', 'nibir', '9/17/2019, 6:38:51 PM');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userlist`
---
 
 CREATE TABLE `userlist` (
   `Id` int(11) NOT NULL,
@@ -57,47 +15,19 @@ CREATE TABLE `userlist` (
   `Handle` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `userlist`
---
 
-INSERT INTO `userlist` (`Id`, `Email`, `Handle`) VALUES
-(1, 'dhrubo9250@gmail.com', 'dhrubo'),
-(3, 'raisulislamnibir24@gmail.com', 'nibir'),
-(4, 'dkjfhgjd@jdshfj.dfkgj', 'ami'),
-(5, 'nibir@m.m', 'nibir1');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `message`
---
 ALTER TABLE `message`
   ADD PRIMARY KEY (`Id`);
 
---
--- Indexes for table `userlist`
---
+
 ALTER TABLE `userlist`
   ADD PRIMARY KEY (`Id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `message`
---
 ALTER TABLE `message`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
---
--- AUTO_INCREMENT for table `userlist`
---
+
 ALTER TABLE `userlist`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
